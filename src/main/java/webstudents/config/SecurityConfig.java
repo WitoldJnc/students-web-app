00739de0129f5e3a916e669d9b35d926d90e68c8
anton <witold.jnc@gmail.com>
2019-02-14 22:36:05 +0700

    add roles, add administrator functional - edit/delete users

--- src/main/java/webstudents/config/SecurityConfig.java
+++ src/main/java/webstudents/config/SecurityConfig.java
@@ -3,56 +3,58 @@ package webstudents.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Bean
-    public AuthenticationProvider authProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-        return provider;
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                    .csrf().disable()
-                    .authorizeRequests()
-                        .antMatchers(
-                            "/login", "/students", "/groups", "/marks","/filter", "/filterLastName", "/disciplines")
-                        .permitAll()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(
+                        "/login", "/students", "/groups", "/marks", "/filter", "/filterLastName", "/disciplines","/registration")
+                .permitAll()
                 //TODO РАЗОБРАТЬСЯ С CSS, ПОЧЕМУ НЕ ПУСКАЕТ РЕГУЛЯРКОЙ  В ДИРЕКТОРИЮ
                 //TODO ДОБАВИТЬ ПУТЬ К СТАТИК ДИРЕКТОРИИ В ПРОПЕРТИ
-                        .antMatchers("/background.css", "/input.css", "/mainIndex.css", "/tables.css").permitAll()
-                        .anyRequest()
-                        .authenticated()
+                .antMatchers("/background.css", "/input.css", "/mainIndex.css", "/tables.css").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                    .formLogin()
-                    .loginPage("/login").permitAll()
+                .formLogin()
+                .loginPage("/login").permitAll()
                 .and()
-                    .logout().invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/").permitAll();
+                .logout().invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").permitAll();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
 }
---
+++ src/main/java/webstudents/models/Role.java
@@ -0,0 +1,38 @@
+package webstudents.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "roles")
+    private String userRole;
+
+    @JsonIgnore
+    @OneToMany(targetEntity = User.class,
+    mappedBy = "roles",cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private List<User> users;
+
+    @Override
+    public String getAuthority() {
+        return userRole;
+    }
+}
